61fc4bc03c3a4d7ae07bcacc3a7713afa789251e
Eric J. Duran <eric.duran7@gmail.com>
2012-09-03 18:26:18 -0400

    Adding basic colorModeData support

--- js/psd.js
+++ js/psd.js
@@ -2,13 +2,15 @@
 psd = function(data) {
   this.dv = new jDataView(data);
   this.header = new psdHeader(this.dv);
+  this.colorModeData = new psdColorModeData(this);
 }
 
 psd.prototype = {}
 
 /**
- * PSD Header Section.
+ * PSD Header Section. // Offset: 0, Length: 26 Bytes
  * The header contains the basic properties of the image
+ *
  * See http://www.adobe.com/devnet-apps/photoshop/fileformatashtml/PhotoshopFileFormats.htm#50577409_19840
  * @param  {jDataView} dv The jDataView object.
  * @return {psdHeader}    The psdHeader object.
@@ -35,5 +37,25 @@ psdHeader.prototype = {
       7: 'Multichannel', 8: 'Duotone', 9: 'Lab'
     };
     return modes[this.color];
+  }
+}
+
+
+/**
+ * PSD Color Mode Data Section. // Offset: 26, Length: Variable.
+ * Only 'Indexed' and 'Duotone' have color mode data.
+ *
+ * See http://www.adobe.com/devnet-apps/photoshop/fileformatashtml/PhotoshopFileFormats.htm#50577409_71638
+ * @param  {jDataView} dv The jDataView object.
+ * @return {psdColorModeData}    The psdColorModeData object.
+ */
+psdColorModeData = function(psd) {
+  this.length = psd.dv.getUint32(26);
+  if (psd.header.colorMode == 'Indexed' || psd.header.colorMode == 'Duotone') {
+    // TODO: Set the proper data for indexed and Duotone color modes.
+  }
+  else {
+    // "For all other modes, this section is just the 4-byte length field, which is set to zero."
+    this.data = psd.dv.getUint32(30);
   }
 }
