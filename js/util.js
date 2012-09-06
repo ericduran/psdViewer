88a9878b2bb0de983b5196107e9b2f545a0f7465
Eric J. Duran <eric.duran7@gmail.com>
2012-09-06 09:33:39 -0400

    Adding some comments to util

--- js/util.js
+++ js/util.js
@@ -10,11 +10,17 @@ var Util = psd.Util = (function UtilClosure() {
   function Util() {}
 
   Util.pad2 = function(num) {
+    // TODO: This fails if the number == 0 since it should be padded to 2 in
+    // case.
     return (num % 2 == 0) ? num : num + 1;
   }
 
   Util.pad4 = function(num) {
-    // This is going to be required for the Pascal string of the layer name.
+    // TODO: This is needed for the Pascal string on the layer name. But I'm
+    // skipping that section for now.
+  }
+
+  Util.rleDecode = function(data) {
   }
 
   return Util;
